d30eda01704337277a3eeb2cd8235cee96fbe198
JettN <122491434+JettN@users.noreply.github.com>
2024-02-08 20:10:27 -0800

    Copy code from lab 5

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/GoalRepository.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/GoalRepository.java
@@ -1,4 +1,19 @@
 package edu.ucsd.cse110.successorator.lib.domain;
 
-public class GoalRepository {
+public interface GoalRepository {
+    /*
+    * Subject<Flashcard> find(int id);
+
+    Subject<List<Flashcard>> findAll();
+
+    void save(Flashcard flashcard);
+
+    void save(List<Flashcard> flashcards);
+
+    void remove(int id);
+
+    void append(Flashcard flashcard);
+
+    void prepend(Flashcard flashcard);
+    * */
 }
