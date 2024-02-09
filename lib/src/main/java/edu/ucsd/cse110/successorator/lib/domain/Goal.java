fdc6a27f8cbf6545769ebc141442f8c10ee3fbf2
Ruben Gonzalez <ruben@Rubens-MacBook-Pro.local>
2024-02-08 21:14:44 -0800

    Added some changes to goal

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Goal.java
@@ -2,8 +2,6 @@ package edu.ucsd.cse110.successorator.lib.domain;
 
 import androidx.annotation.Nullable;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Objects;
 
 /**
@@ -13,18 +11,21 @@ import java.util.Objects;
  * done The completion status of the goal; true if the goal is completed, false otherwise.
  */
 public class Goal {
-    private final @Nullable String title;
     private final @Nullable Integer id;
-    private final boolean done;
+    private final @Nullable String title;
+    private final boolean isComplete;
+    private final Integer sortOrder; // Changed from 'int' to 'Integer' to allow null values.
 
     public Goal(
             @Nullable String title,
             @Nullable Integer id,
-            boolean done
+            boolean isComplete,
+            @Nullable Integer sortOrder // Corrected parameter type and removed the semicolon.
     ) {
         this.id = id;
         this.title = title;
-        this.done = done;
+        this.isComplete = isComplete;
+        this.sortOrder = sortOrder;
     }
 
     @Nullable
@@ -37,24 +38,28 @@ public class Goal {
         return title;
     }
 
-    public boolean isDone() {
-        return done;
+    public boolean isComplete() {
+        return isComplete;
+    }
+
+    @Nullable
+    public Integer getSortOrder() {
+        return sortOrder;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Goal goal = (Goal) o;
-        return done == goal.done &&
+        return isComplete == goal.isComplete &&
                 Objects.equals(id, goal.id) &&
-                Objects.equals(title, goal.title);
+                Objects.equals(title, goal.title) &&
+                Objects.equals(sortOrder, goal.sortOrder);
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(id, title, done);
+        return Objects.hash(id, title, isComplete, sortOrder);
     }
 }
