6f5d9e96f64980f233d0fbae0e50925ac6c2da2a
etw0001 <122562296+etw0001@users.noreply.github.com>
2024-02-13 17:18:48 -0800

    test

---
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/DateTest.java
@@ -0,0 +1,17 @@
+package edu.ucsd.cse110.successorator.lib.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTest {
+    @Test
+    public void testGetters() {
+        LocalDateTime testDate = LocalDateTime.of(2024, 2, 13, 12, 21);
+        Date date = new Date(testDate, DateTimeFormatter.ofPattern("EEEE M/dd"));
+        assertEquals("Tuesday 2/13", date.getDate());
+    }
+}
