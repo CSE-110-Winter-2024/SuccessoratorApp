9606445b8696e49bd8d9477c65a974edf7181bdd
etw0001 <122562296+etw0001@users.noreply.github.com>
2024-02-13 17:23:45 -0800

    documentation link

--- lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Date.java
+++ lib/src/main/java/edu/ucsd/cse110/successorator/lib/domain/Date.java
@@ -3,6 +3,10 @@ package edu.ucsd.cse110.successorator.lib.domain;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * https://docs.oracle.com/javase/8/docs/api/java/time/LocalDateTime.html
+ * https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+ */
 public class Date {
     private LocalDateTime dateTime;
     private DateTimeFormatter formatter;
