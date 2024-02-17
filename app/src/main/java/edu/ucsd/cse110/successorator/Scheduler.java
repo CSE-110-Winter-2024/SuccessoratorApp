389cb537498ade9b26ade06720268dc9c7ae752f
etw0001 <122562296+etw0001@users.noreply.github.com>
2024-02-17 12:39:06 -0800

    change display date at 2am of next day

---
+++ app/src/main/java/edu/ucsd/cse110/successorator/Scheduler.java
@@ -0,0 +1,31 @@
+package edu.ucsd.cse110.successorator;
+
+import android.widget.TextView;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import edu.ucsd.cse110.successorator.lib.domain.Date;
+
+// https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/ScheduledExecutorService.html
+public class Scheduler {
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final TextView dateText;
+
+    public Scheduler(TextView dateText) {
+        this.dateText = dateText;
+    }
+
+    public void startTask() {
+        scheduler.scheduleAtFixedRate(() -> {
+            LocalDateTime currentTime = LocalDateTime.now().minusHours(2);
+            if (currentTime.getHour() == 0 && currentTime.getMinute() == 0) {
+                Date date = new Date(LocalDateTime.now(), DateTimeFormatter.ofPattern("EEEE M/dd"));
+                dateText.setText(date.getDate());
+            }
+        }, 0, 1, TimeUnit.MINUTES);
+    }
+}
