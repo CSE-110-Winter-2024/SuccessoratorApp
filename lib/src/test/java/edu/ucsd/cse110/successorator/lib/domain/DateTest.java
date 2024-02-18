31b3739e27d978b8be7b0f7c5ec8c58cf942d944
JettN <122491434+JettN@users.noreply.github.com>
2024-02-18 13:38:31 -0800

    Wrote test for advanceDate function

--- lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/DateTest.java
+++ lib/src/test/java/edu/ucsd/cse110/successorator/lib/domain/DateTest.java
@@ -15,4 +15,17 @@ public class DateTest {
         date.setDate(testDate);
         assertEquals("Tuesday 2/13", date.formatDate());
     }
+
+    @Test
+    public void testAdvanceDate(){
+        LocalDateTime testDate = LocalDateTime.of(2024, 2, 13, 12, 21);
+        Date actual = new Date(DateTimeFormatter.ofPattern("EEEE M/dd"));
+        actual.setDate(testDate);
+        actual.advanceDate();
+
+        LocalDateTime expectedDate = LocalDateTime.of(2024, 2, 13 + 1, 12, 21);
+        Date expected = new Date(DateTimeFormatter.ofPattern("EEEE M/dd"));
+        expected.setDate(expectedDate);
+        assertEquals(actual.getDate(), expected.getDate());
+    }
 }
